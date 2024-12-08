a21df32b4c16d577741313268b8fd719f394aa7b
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-12-08 17:41:55 +0100

    update

--- money.py
+++ money.py
@@ -9,4 +9,13 @@ df['Started Date'] = pd.to_datetime(df['Started Date'])
 df['Year'] = df['Started Date'].dt.year
 df['Month'] = df['Started Date'].dt.month
 df['Day'] = df['Started Date'].dt.day
-print(df)
+
+this_year = df[df['Year'] == 2024]
+
+
+last_month = this_year[this_year['Month'] == 11]
+print(last_month)
+
+food_last_month =  last_month[last_month['Category'] == 'Food']
+sum_food_last_month = food_last_month['Amount'].sum()
+print(f"Total amount for Food: {sum_food_last_month}")
